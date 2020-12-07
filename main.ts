4d155f7002c9004e9d49a430d9ebfeed6e64c7e7
NatashaG6 <74411619+NatashaG6@users.noreply.github.com>
2020-12-07 16:36:02 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    basic.pause(1000)
+    if (input.temperature() >= 30) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (input.temperature() < 25) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
