1e79f74662113f54bc5835c6dd825e8b196c4ef5
NatashaG6 <74411619+NatashaG6@users.noreply.github.com>
2021-01-12 14:14:36 +0000

    updated

--- main.ts
+++ main.ts
@@ -8,8 +8,8 @@ basic.forever(function () {
     basic.showNumber(input.temperature())
     basic.pause(1000)
     if (input.temperature() >= 30) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
     } else if (input.temperature() < 25) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
